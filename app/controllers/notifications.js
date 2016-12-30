b72362f1058755da13568736671398f1ede98861
doowb <brian.woodward@gmail.com>
2016-12-30 14:00:11 -0500

    use custom scopes to query notifications when set on query string calculate categories and counts for side nav

--- app/controllers/notifications.js
+++ app/controllers/notifications.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var co = require('co');
 var express = require('express');
+var models = require('../models');
 
 module.exports = function(config) {
   return function(app) {
@@ -10,20 +12,59 @@ module.exports = function(config) {
         res.redirect('/login');
         return;
       }
-      req.user.sync(function(err) {
-        if (err) return next(err);
-        req.user
-          .getNotifications()
-          .then(function(notifications) {
-            res.render('notifications', {
-              title: 'Notifications',
-              notifications: notifications
-            });
-          })
-          .catch(next);
-      });
+
+      var q = req.query;
+      var scopes = [];
+
+      if (q['starred']) {
+        scopes.push('starred');
+      } else if (q['archived']) {
+        scopes.push('archived');
+      } else {
+        scopes.push('inbox');
+      }
+
+      co(function*() {
+        var counter = getCounts(req.user, scopes);
+
+        var context = {title: 'Notifications'};
+        context['types'] = yield counter('subject_type');
+        context['statuses'] = yield counter('unread');
+        context['reasons'] = yield counter('reason');
+        context['unread_repositories'] = yield counter('repository_full_name');
+
+        var subScopes = ['repo', 'reason', 'type', 'status', 'owner'];
+        subScopes.forEach(function(scope) {
+          if (q.hasOwnProperty(scope)) {
+            scopes.push({method: [scope, q[scope]]});
+          }
+        });
+
+        context['notifications'] = yield req.user.getNotifications({scope: scopes});
+
+        res.render('notifications', context);
+      })
+      .catch(next);
     });
 
     return router;
   };
+
+  function getCounts(user, scopes) {
+    return function(col) {
+      return models.Notification
+        .count({
+          attributes: [col],
+          scope: scopes,
+          where: {user_id: user.id},
+          group: col
+        })
+        .then(function(rows) {
+          return rows.reduce(function(acc, row) {
+            acc[row[col]] = row.count;
+            return acc;
+          }, {});
+        });
+    };
+  }
 }
