f3f6fb861e30cd8ac245f0d2da3f85d88086f34f
doowb <brian.woodward@gmail.com>
2016-12-30 18:47:12 -0500

    read in partial paths so consolidate doesn't screw them up

--- app/controllers/notifications.js
+++ app/controllers/notifications.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var fs = require('fs');
 var co = require('co');
+var path = require('path');
 var express = require('express');
 var models = require('../models');
+var helpers = require('../helpers');
 
 module.exports = function(config) {
   return function(app) {
@@ -13,7 +16,13 @@ module.exports = function(config) {
         return;
       }
 
+
       var q = req.query;
+
+      // re-register the helpers with handlebars with the current params
+      helpers.application(app, q);
+      helpers.notifications(app, q);
+
       var scopes = [];
 
       if (q['starred']) {
@@ -25,20 +34,25 @@ module.exports = function(config) {
       }
 
       co(function*() {
-        var counter = getCounts(req.user, scopes);
-
-        var context = {title: 'Notifications'};
-        context['types'] = yield counter('subject_type');
-        context['statuses'] = yield counter('unread');
-        context['reasons'] = yield counter('reason');
-        context['unread_repositories'] = yield counter('repository_full_name');
+        var context = {
+          title: 'Notifications',
+          partials: getPartials(app),
+          params: q
+        };
 
         var subScopes = ['repo', 'reason', 'type', 'status', 'owner'];
         subScopes.forEach(function(scope) {
           if (q.hasOwnProperty(scope)) {
-            scopes.push({method: [scope, q[scope]]});
+            var val = q[scope];
+            scopes.push({method: [scope, val]});
           }
         });
+
+        var counter = getCounts(req.user, scopes);
+        context['types'] = yield counter('subject_type');
+        context['statuses'] = yield counter('unread');
+        context['reasons'] = yield counter('reason');
+        context['unread_repositories'] = yield counter('repository_full_name');
 
         context['notifications'] = yield req.user.getNotifications({scope: scopes});
 
@@ -53,9 +67,10 @@ module.exports = function(config) {
   function getCounts(user, scopes) {
     return function(col) {
       return models.Notification
+        .scope(scopes)
         .count({
           attributes: [col],
-          scope: scopes,
+          // scope: scopes,
           where: {user_id: user.id},
           group: col
         })
@@ -66,5 +81,15 @@ module.exports = function(config) {
           }, {});
         });
     };
+  }
+
+  function getPartials(app) {
+    var partials = fs.readdirSync(path.join(app.get('views'), 'partials'));
+    return partials.reduce(function(acc, filename) {
+      var ext = path.extname(filename);
+      var basename = path.basename(filename, ext);
+      acc[basename] = path.join('partials', basename);
+      return acc;
+    }, {});
   }
 }
