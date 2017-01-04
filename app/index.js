917306ad28bd696ff7f253fe2559956b9e1c79c3
doowb <brian.woodward@gmail.com>
2017-01-03 19:34:44 -0500

    make the main app an exportable function that takes a config object so it can be passed in

--- app/index.js
+++ app/index.js
@@ -8,59 +8,60 @@ var consolidate = require('consolidate');
 consolidate.requires.handlebars = require('handlebars');
 var cookieParser = require('cookie-parser');
 
-var config = require('../config');
-var app = express();
+module.exports = function(config) {
+  var app = express();
 
-app.set('url', config.url || 'http://localhost:3000');
-app.set('root_path', '/');
+  app.set('url', config.url || 'http://localhost:3000');
+  app.set('root_path', '/');
 
-// view engine setup
-app.set('handlebars', consolidate.requires.handlebars);
-app.engine('hbs', consolidate.handlebars);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+  // view engine setup
+  app.set('handlebars', consolidate.requires.handlebars);
+  app.engine('hbs', consolidate.handlebars);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require('./routes')(config);
-routes(app);
+  var routes = require('./routes')(config);
+  routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handlers
+  // error handlers
 
-// development error handler
-// will print stacktrace
-if(app.get('env') === 'development') {
+  // development error handler
+  // will print stacktrace
+  if(app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
+  return app;
+};
