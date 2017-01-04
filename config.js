f6a280a14b16a44010f864d1a23211510c35ecf4
doowb <brian.woodward@gmail.com>
2017-01-03 19:58:46 -0500

    move all config handling to config.js and default to environment variables when not available

--- config.js
+++ config.js
@@ -1,13 +1,33 @@
 'use strict';
 
+var extend = require('extend-shallow');
 var exists = require('fs-exists-sync');
+
+var db = {};
 var config = {};
 if (exists('./config.json')) {
   config = require('./config.json');
 } else {
   console.log();
-  console.log('[WARNING]:', 'no `config.json` file found.');
+  console.log('[WARNING]:', 'no `./config.json` file found.');
   console.log();
 }
+
+if (exists('./db/config.json')) {
+  db = require('./db/config.json');
+} else {
+  db = {use_env_variable: 'DATABASE_URL'};
+  console.log();
+  console.log('[WARNING]:', 'no `./db/config.json` file found.');
+  console.log();
+}
+
+config.env = process.env.NODE_ENV || 'development';
+config = extend({
+  'url': 'http://localhost:3000',
+  'GITHUB_CLIENT_ID': process.env.GITHUB_CLIENT_ID || '',
+  'GITHUB_CLIENT_SECRET': process.env.GITHUB_CLIENT_SECRET || '',
+  db: db[config.env] || db
+}, config);
 
 module.exports = config;
